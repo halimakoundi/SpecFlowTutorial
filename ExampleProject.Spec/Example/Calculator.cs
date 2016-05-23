8e68bd4362ed4b6f1422a8cefabd009373efceca
Halima KOUNDI <hkoundi@gmail.com>
2016-05-23 11:38:53 +0100

    tutorial bdd with specflow and NUnit

---
+++ ExampleProject.Spec/Example/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Example
+{
+    public class Calculator
+    {
+        public int FirstNumber {private get; set; }
+        public int SecondNumber  { private get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
